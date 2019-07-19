d6e1e8afb41866a4e731cf44a4473e1c0d8a266f
EL MERROUNI Marouane <melmerrouni@norsys.fr>
2019-07-19 15:37:28 +0100

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# search container
+containerName = $1
+dockerImageTag = $2  
+docker ps -f name=$containerName | grep -w $containerName
+
+if [ $0 -eq 0 ];then
+	docker kill $containerName > /dev/null 2>&1
+	docker rm $containerName > /dev/null 2>&1
+	docker run -d $dockerImageTag --name api-reservation
+else
+	docker run -d $dockerImageTag --name api-reservation
+fi
