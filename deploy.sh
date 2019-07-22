5fc3caf4db414603150b3b518d0b1ff8bdce6a4e
EL MERROUNI Marouane <melmerrouni@norsys.fr>
2019-07-22 17:53:15 +0100

    Config project bi columns

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker ps -f name=$containerName | grep -w $containerName
 if [ $0 -eq 0 ];then
 	docker kill $containerName > /dev/null 2>&1
 	docker rm $containerName > /dev/null 2>&1
-	docker run -d $dockerImageTag --name api-reservation
+	docker run -d $dockerImageTag -name api-reservation
 else
-	docker run -p 8088:8088 -d $dockerImageTag --name api-reservation
+	docker run -p 8088:8088 -d $dockerImageTag -name api-reservation
 fi
