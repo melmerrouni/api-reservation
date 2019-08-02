a01334cb977d40224640131d11ea83034c30bcc2
EL MERROUNI Marouane <37534710+melmerrouni@users.noreply.github.com>
2019-08-02 17:57:45 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ containerName=$1
 dockerImageTag=$2  
 docker ps -f name=$containerName | grep -w $containerName
 
-if [ $0 -eq 0 ];then
+if [ $? -eq 0 ];then
 	docker kill $containerName > /dev/null 2>&1
 	docker rm $containerName > /dev/null 2>&1
 	docker run --name api-reservation -p 8088:8088 -d $dockerImageTag
